0855b8860a49de7542942311c9818542b48e5640
Luís Renato Moreira da Costa <luisrenatocosta@hotmail.com>
2025-04-27 22:14:50 -0300

    Atualização

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,18 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+        System.out.println("TV Ligada?" + smartTv.ligada);
+        System.out.println("Canal Atual:" + smartTv.canal);
+        System.out.println("Volume Atual:" + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo status - TV Ligada?" + smartTv.ligada);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        smartTv.mudarCanal(15);  
+    }
+}
